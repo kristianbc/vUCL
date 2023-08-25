ece4c86fd5e290091188f92dc03909938fbe53dd
Kristian <58121983+kristianbc@users.noreply.github.com>
2023-08-25 15:15:06 +0200

    fixes

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('details-form');
-    
+    let lastSubmitTime = 0; // Initialize with 0 to allow the first submission
+  
     form.addEventListener('submit', function(event) {
       event.preventDefault();
+  
+      const currentTime = Date.now();
+      const timeSinceLastSubmit = currentTime - lastSubmitTime;
+      const oneMinute = 60 * 1000; // 1 minute in milliseconds
+  
+      if (timeSinceLastSubmit < oneMinute) {
+        const remainingTime = (oneMinute - timeSinceLastSubmit) / 1000;
+        alert(`Please wait ${remainingTime.toFixed(1)} seconds before submitting again.`);
+        return;
+      }
   
       const name = document.getElementById('name').value;
       const subject = document.getElementById('subject').value;
       const details = document.getElementById('details').value;
   
       const data = {
-        name,
-        subject,
-        details
+        content: '```\n' +
+                 `Subject ID: (ID: ${generateUniqueId()}):\n` +
+                 `Name: ${name}\n` +
+                 '----------------------\n' +
+                 `Subject: ${subject}\n` +
+                 '----------------------\n' +
+                 `Details: ${details}\n` +
+                 '```'
       };
   
-      // Replace 'DISCORD_WEBHOOK_URL' with your actual Discord webhook URL
       const webhookURL = 'https://discord.com/api/webhooks/1144594322453450862/CjOz5t0pdRHkMwCEGD0NGiZy3ZPao8tNRf1Z501n8CIkH7CCrtWikdYfgMDHEOoCfXxY';
   
       fetch(webhookURL, {
@@ -27,8 +42,12 @@ document.addEventListener('DOMContentLoaded', function() {
       .then(response => {
         if (response.ok) {
           alert('Details submitted successfully!');
+          lastSubmitTime = currentTime; // Update last submit time
         } else {
-          alert('An error occurred while submitting details.');
+          response.json().then(errorData => {
+            console.error('Error:', errorData);
+            alert('An error occurred while submitting details: ' + errorData.message);
+          });
         }
       })
       .catch(error => {
@@ -37,4 +56,8 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  
+  function generateUniqueId() {
+    return Math.random().toString(36).substr(2, 9);
+  }
   
